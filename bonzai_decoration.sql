83195badef4d5e4954e9a2e1cbf0cdb5a5db8f62
xcodeme21 <xcodeme21@gmail.com>
2021-01-02 11:41:44 +0700

    update database

--- bonzai_decoration.sql
+++ bonzai_decoration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 15, 2020 at 02:17 PM
+-- Generation Time: Jan 02, 2021 at 05:41 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -42,7 +42,8 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`id`, `nama_client`, `email_client`, `telepon_client`, `alamat_client`, `created_at`, `updated_at`) VALUES
-(1, 'PT.  Alpha Partner Solution', 'xcodeme21@gmail.com', '021-56789', 'Jalan raya lenteng agung no.156, Kel. Lenteng Agung, Kec. Jagakarsa, Jakarta Selatan', '2020-12-12 19:40:46', '2020-12-12 19:40:46');
+(1, 'PT.  Alpha Partner Solution', 'xcodeme21@gmail.com', '021-56789', 'Jalan raya lenteng agung no.156, Kel. Lenteng Agung, Kec. Jagakarsa, Jakarta Selatan', '2020-12-12 19:40:46', '2020-12-12 19:40:46'),
+(3, 'PT. Alyce', 'admin@alyce.com', '021-6789873', 'Jakarta Selatan', '2020-12-19 05:04:48', '2020-12-19 05:04:48');
 
 -- --------------------------------------------------------
 
@@ -59,6 +60,61 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `kode_schedule` varchar(50) NOT NULL,
+  `no_invoice` varchar(255) NOT NULL,
+  `tanggal_invoice` date NOT NULL,
+  `dp` bigint(20) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_batal` date DEFAULT NULL,
+  `keterangan_batal` text DEFAULT NULL,
+  `nominal_total` bigint(20) NOT NULL DEFAULT 0,
+  `nominal_terbayar` bigint(20) NOT NULL DEFAULT 0,
+  `status_invoice` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`id`, `kode_schedule`, `no_invoice`, `tanggal_invoice`, `dp`, `keterangan`, `tanggal_batal`, `keterangan_batal`, `nominal_total`, `nominal_terbayar`, `status_invoice`, `created_at`, `updated_at`) VALUES
+(1, 'BNZ/SCH/2021/01/FM1BZ', 'BNZ/INV/2021/01/7TIEU', '2021-01-02', 3000000, '<p>DP awalan</p>', NULL, NULL, 10000000, 10000000, 1, '2021-01-01 17:16:48', '2021-01-01 20:05:06'),
+(2, 'BNZ/SCH/2021/01/RF53B', 'BNZ/INV/2021/01/FXHBE', '2021-01-02', 3000000, '<p>-</p>', NULL, NULL, 10000000, 3000000, 0, '2021-01-01 20:31:58', '2021-01-01 20:31:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kwitansi`
+--
+
+CREATE TABLE `kwitansi` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL DEFAULT 0,
+  `kode_kwitansi` varchar(255) NOT NULL,
+  `tanggal_kwitansi` date NOT NULL,
+  `nominal_pembayaran` bigint(20) NOT NULL,
+  `keterangan_pembayaran` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kwitansi`
+--
+
+INSERT INTO `kwitansi` (`id`, `id_invoice`, `kode_kwitansi`, `tanggal_kwitansi`, `nominal_pembayaran`, `keterangan_pembayaran`, `created_at`, `updated_at`) VALUES
+(4, 1, 'BNZ/KWI/2021/01/QIBNV', '2021-01-02', 5000000, '<p>nyicil</p>', '2021-01-01 19:58:42', '2021-01-01 19:58:42'),
+(6, 1, 'BNZ/KWI/2021/01/Q7J83', '2021-01-02', 2000000, '-', '2021-01-01 20:05:06', '2021-01-01 20:05:06');
 
 -- --------------------------------------------------------
 
@@ -102,7 +158,8 @@ CREATE TABLE `package_decoration` (
 --
 
 INSERT INTO `package_decoration` (`id`, `nama_paket`, `harga_paket`, `item_paket`, `keterangan_paket`, `created_at`, `updated_at`) VALUES
-(2, 'Paket A', 10000000, '<p>adhahdjkahdjk</p>\r\n\r\n<p>aldlakd</p>\r\n\r\n<ol>\r\n	<li>ajdlkajdl</li>\r\n	<li>lkajdlkajd</li>\r\n	<li>a;djkaljdlkajd</li>\r\n</ol>', '<p>fkshfkjskfhsjkf</p>\r\n\r\n<p>;a;hdkjadkahdjkahdjkad</p>', '2020-12-15 05:02:18', '2020-12-15 05:06:41');
+(2, 'Paket A', 10000000, '<p>adhahdjkahdjk</p>\r\n\r\n<p>aldlakd</p>\r\n\r\n<ol>\r\n	<li>ajdlkajdl</li>\r\n	<li>lkajdlkajd</li>\r\n	<li>a;djkaljdlkajd</li>\r\n</ol>', '<p>fkshfkjskfhsjkf</p>\r\n\r\n<p>;a;hdkjadkahdjkahdjkad</p>', '2020-12-15 05:02:18', '2020-12-15 05:06:41'),
+(3, 'Paket B', 15000000, '<ol>\r\n	<li>dajdkadhjka</li>\r\n	<li>jhadkhakjdh</li>\r\n	<li>hadkjhajkdh</li>\r\n	<li>dakdjhjk</li>\r\n	<li>&nbsp;</li>\r\n</ol>', '<p>testing keterangan</p>', '2020-12-19 05:06:05', '2020-12-19 05:06:05');
 
 -- --------------------------------------------------------
 
@@ -140,7 +197,8 @@ CREATE TABLE `schedule` (
 --
 
 INSERT INTO `schedule` (`id`, `kode_schedule`, `client_id`, `tanggal_schedule`, `tanggal_selesai`, `package_decoration_id`, `venue`, `status`, `created_at`, `updated_at`) VALUES
-(2, '2020/12/SCH/G4XE0', 1, '2020-12-29', '2020-12-31', 2, '<ol>\r\n	<li>akjdjkahdkad</li>\r\n	<li>adhakjdhakd</li>\r\n	<li>ahdkjadkad</li>\r\n	<li>lajkhdjkahkdjahd</li>\r\n</ol>', 1, '2020-12-15 05:54:05', '2020-12-15 06:06:55');
+(1, 'BNZ/SCH/2021/01/FM1BZ', 3, '2021-01-02', '2021-01-12', 2, '<p>kjadljald</p>\r\n\r\n<p>ajdkjaldj</p>', 2, '2021-01-01 16:42:18', '2021-01-01 20:05:06'),
+(2, 'BNZ/SCH/2021/01/RF53B', 1, '2021-01-28', '2021-01-30', 2, '<p>kjakdjakldjlakd</p>', 1, '2021-01-01 16:56:49', '2021-01-01 20:31:58');
 
 -- --------------------------------------------------------
 
@@ -184,6 +242,18 @@ ALTER TABLE `failed_jobs`
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kwitansi`
+--
+ALTER TABLE `kwitansi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -222,13 +292,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kwitansi`
+--
+ALTER TABLE `kwitansi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -240,7 +322,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `package_decoration`
 --
 ALTER TABLE `package_decoration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `schedule`
